be409d9965bd4109723da0b0fd793b40f2b20301
Guilherme Rossato <guilhermrossato@gmail.com>
2020-06-05 06:40:48 -0300

    Added basic file dropper functionality

--- src/app/components/file-dropper/file-dropper.component.ts
+++ src/app/components/file-dropper/file-dropper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-file-dropper',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileDropperComponent implements OnInit {
 
+  @Output() imageAdded = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onFileSelect(event: Event, target: HTMLInputElement) {
+    const files = target.files;
+
+    for (let i = files.length-1; i >= 0; i--) {
+      const file = files[i];
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        const url = e.target.result;
+        if (typeof url !== "string" || !url) {
+          throw new Error(`Unexpected URL when opening file "${file.name}"`);
+        }
+        this.imageAdded.emit(url);
+      }
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onDragOver(event: Event, dataTransfer: DataTransfer) {
+    event.stopPropagation();
+    event.preventDefault();
+    dataTransfer.dropEffect = 'copy';
+  }
 }
