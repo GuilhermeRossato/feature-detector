1f28d9a069e63a2776be03507fe9c652906e26a3
Guilherme Rossato <guilhermrossato@gmail.com>
2020-06-16 16:15:27 -0300

    Added include dataset border in network actions

--- src/app/components/network-actions/network-actions.component.ts
+++ src/app/components/network-actions/network-actions.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NetworkActionsComponent implements OnInit {
 
+  public includeBorderOnDataset = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onIncludeBorderChange({name, value}: {name: string, value: 'true' | 'false'}) {
+    this.includeBorderOnDataset = value === 'true';
   }
 
 }
